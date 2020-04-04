04658f6e7f803063d3bab8b3786161cb97fa6293
Murat Kızılöz <muratkizilozz@gmail.com>
2020-04-04 18:18:35 +0300

    express js add project

---
+++ routes/movie.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+//Models
+
+const Movie = require('../models/Movie');
+
+// post method
+router.post('/', (req, res, next) => {
+ //  const { title, imdb_score, category, country, year } = req.body;
+
+  const movie = new Movie(req.body);
+
+ // const movie = new Movie({
+ //   title: title,
+ //   imdb_score: imdb_score,
+ //   category: category,
+ //   country: country,
+ //   year: year
+ // });
+
+
+ movie.save((err, data) => {
+   if (err)
+      res.json(err);
+   res.json(data)
+ });
+});
+
+module.exports = router;
