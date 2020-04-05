207bd1e4e4ecd3afb3d4df1f112157a36bc78ee4
Murat Kızılöz <muratkizilozz@gmail.com>
2020-04-05 16:32:41 +0300

    api/directors & api/directors/:director_id [GET]

--- routes/director.js
+++ routes/director.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -5,9 +6,109 @@ const router = express.Router();
 
 const Director = require('../models/Director');
 
+// director oluşturma (post) metodu
 router.post('/', (req, res, next) => {
   const director = new Director(req.body);
   const promise = director.save();
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err)
+  })
+});
+
+// tüm yönetmenleri çekme (get işlemi)
+router.get('/', (req, res) => {
+  const promise = Director.aggregate([
+    {
+      $lookup: {
+        from: 'movies', // collection adı
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies', // dönen datanın atanacağı yer
+      }
+    },
+    {
+      $group : {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movies: {
+          $push: '$movies'
+        }
+      }
+    },
+    {
+      $unwind: {
+        path: '$movies',
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        movies: '$movies'
+      }
+    }
+  ]);
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err)
+  })
+});
+
+// id ye göre yönetmen listeleme
+router.get('/:director_id', (req, res) => {
+  const promise = Director.aggregate([
+    {
+      $match: {
+        '_id': mongoose.Types.ObjectId(req.params.director_id)
+      }
+    },
+    {
+      $lookup: {
+        from: 'movies', // collection adı
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies', // dönen datanın atanacağı yer
+      }
+    },
+    {
+      $group : {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movies: {
+          $push: '$movies'
+        }
+      }
+    },
+    {
+      $unwind: {
+        path: '$movies',
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        movies: '$movies'
+      }
+    }
+  ]);
 
   promise.then((data) => {
     res.json(data);
