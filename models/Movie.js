5dad74189fd93806566d333f770a89eefd745cdd
Murat Kızılöz <muratkizilozz@gmail.com>
2020-04-06 17:55:15 +0300

    api movies validation control

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,32 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title: {
         type: String,
-        required: true
+        // hata mesajı olarak kendi yazdığımız mesajı gönderme şekli
+        required: [true, '`{PATH}` alanı zorunludur.'],
+        maxlength: [50, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır'],
+        minlength: [1, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden büyük olmalıdır'],
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: {
+      type: String ,
+      maxlength: 30,
+      minlength: 2,
+    },
+    country: {
+        type: String ,
+        maxlength: 30,
+        minlength: 2,
+    },
+    year: {
+        type: Number,
+        min: 1900,
+        max:2025
+
+    },
+    imdb_score: {
+        type: Number,
+        max: 10,
+        min: 0
+    },
     createdAt: {
         type: Date,
         default: Date.now
