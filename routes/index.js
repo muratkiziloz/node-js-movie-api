2d830e0ae60816e9cb47c7952f7a4812f29bf8ec
Murat Kızılöz <muratkizilozz@gmail.com>
2020-04-06 21:11:32 +0300

    start script

--- routes/index.js
+++ routes/index.js
@@ -60,7 +60,7 @@ router.post('/authenticate', (req, res) => {
                             username: username
                         };
                         const token = jwt.sign(payload, req.app.get('api_secret_key'), {
-                            expiresIn: 720 // 12 saat
+                            expiresIn: 2880 // 48 saat
                         });
 
                         res.json({
