e66ed07a2852e0318c62e9be3d8be38105b9dd68
Murat Kızılöz <muratkizilozz@gmail.com>
2020-04-06 17:57:40 +0300

    api directors validation control

--- models/Director.js
+++ models/Director.js
@@ -3,9 +3,21 @@ const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
 
-    name: String,
-    surname: String,
-    bio: String,
+    name: {
+        type: String,
+        maxlength: 50,
+        minlength:2
+    },
+    surname: {
+        type: String,
+        maxlength: 50,
+        minlength: 2
+    },
+    bio: {
+        type: String,
+        maxlength: 2000,
+        minlength: 60
+    },
     createdAt: {
         type: Date,
         default: Date.now
