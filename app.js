beb3959dd57b8a3308ac16b9f98a0d7d9f044085
Murat Kızılöz <muratkizilozz@gmail.com>
2020-04-07 13:10:18 +0300

    access allow origin cors edit5

--- app.js
+++ app.js
@@ -56,20 +56,24 @@ app.use((err, req, res, next) => {
 });
 
 
-const corsOptions = {
-  origin: 'https://node-filmrafi-api.herokuapp.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-// app.get('/products/:id', cors(corsOptions), function (req, res, next) {
-//   res.json({msg: 'This is CORS-enabled for only example.com.'})
+// const corsOptions = {
+//   origin: 'https://node-filmrafi-api.herokuapp.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+//
+// // app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+// //   res.json({msg: 'This is CORS-enabled for only example.com.'})
+// // });
+//
+// app.get("*", function(req, res) {
+//   res.send("sms app server status: RUNNING");
 // });
 
-app.get("*", function(req, res) {
-  res.send("sms app server status: RUNNING");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://node-filmrafi-api.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
-
-
 
 
 module.exports = app;
