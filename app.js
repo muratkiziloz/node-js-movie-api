75b8df6c2af0f1d5b10939d1533481bad1fb755c
Murat Kızılöz <muratkizilozz@gmail.com>
2020-04-07 13:27:40 +0300

    access allow origin cors edit5

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const config = require('./config');
 app.set('api_secret_key', config.api_secret_key);
 
 // Middleware
-app.use(cors());
 const verifyToken = require('./middleware/verify-token');
+app.use(cors());
 
 
 // view engine setup
@@ -53,25 +53,6 @@ app.use((err, req, res, next) => {
   // render the error page
   res.status(err.status || 500);
   res.json({ error:{ message: err.message, code: err.code } });
-});
-
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
 });
 
 
